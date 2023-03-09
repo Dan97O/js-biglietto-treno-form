f589100b03ee849d29f12e203e8811ee43eb3e89
Dan97O <dan97-oddo@live.it>
2023-03-09 16:11:21 +0100

    add edit finalprice js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,23 +14,23 @@ document.getElementById('btn_upload').addEventListener('click', function() {
     console.log(userAge);
     
     // calcolare il prezzo del viaggio 1km = 0.21€
-    const price_per_km = kilometersToGo * 0.21
+    const price_per_km = 0.21
     const priceTicket = kilometersToGo * price_per_km
     console.log(priceTicket);
+
  
 
     if(userAge < 18){
-        const priceTicketYoung = (priceTicket / 100 * 20).toFixed(2);
-        console.log(priceTicketYoung);
-   
-        document.getElementById("total_price").innerHTML = (`il tuo prezzo Giovani é ${priceTicketYoung} €`);
+        const priceTicketYoung = (priceTicket * 0.2).toFixed(2);
+        const finalprice = (priceTicket - priceTicketYoung).toFixed(2); 
+        console.log(finalprice);
+        document.getElementById("total_price").innerHTML = (`il tuo prezzo Giovani é ${finalprice} €`);
     } else if(userAge > 65){
-        const priceTicketOld = (priceTicket / 100 * 40).toFixed(2);
+        const priceTicketOld = (priceTicket * 0.4).toFixed(2);
+        const finalprice = (priceTicket - priceTicketOld).toFixed(2); 
         console.log(priceTicketOld);
-        
-        document.getElementById("total_price").innerHTML = (`il tuo prezzo Anziani é ${priceTicketOld} €`);
+        document.getElementById("total_price").innerHTML = (`il tuo prezzo Anziani é ${finalprice} €`);
     } else{
-        
         document.getElementById("total_price").innerHTML = (`il tuo prezzo Normale é ${priceTicket} €`);
     }  
   })
